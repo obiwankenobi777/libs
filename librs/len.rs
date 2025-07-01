20f09341df7d4d33d8d57274a5debf063e05050a
obiwankenobi777 <ordemjedi7@gmail.com>
2025-07-01 18:55:58 -0300

    librs

---
+++ librs/len.rs
@@ -0,0 +1,17 @@
+use std::env;
+fn main() {
+    let args:Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Erro!\nParemetros Ausentes!");
+        std::process::exit(1);
+    }
+
+    let mut sum: usize = 0;
+    for (i, j) in args.iter().enumerate().skip(1) {
+        println!("[{}] {} -> {}", i, j, j.len());
+        sum += j.len();
+    }
+    println!("\nwords -> {}", args.len() - 1);
+    println!("characters -> {}", sum);
+}
