1c89f43ad4a0d18315476c701eb4f85975baeb45
obiwankenobi777 <ordemjedi7@gmail.com>
2025-07-17 14:34:11 -0300

    ./librs/check.rs

--- librs/gcd.rs
+++ librs/gcd.rs
@@ -9,21 +9,21 @@ fn gcd(u: u128, v: u128) -> u128 {
 } 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Erro\nParametros Ausentes");
+    if args.len() < 3 {
+        eprintln!("ERROR!\nTwo parameters are required!");
         std::process::exit(1);
     }
     let u = match args[1].trim().parse::<u128>() {
         Ok(n) => n,
         Err(_) => {
-            eprintln!("Erro: argumento '{}' invalido", args[1]);
+            eprintln!("ERROR!\nInvalid Argument: {}", args[1]);
             std::process::exit(1);
         },
     };
     let v = match args[2].trim().parse::<u128>() {
         Ok(n) => n,
         Err(_) => {
-            eprintln!("Erro: argumento '{}' invalido", args[2]);
+            eprintln!("ERROR!\nInvalid Argument: {}", args[2]);
             std::process::exit(1);
         },
     };
