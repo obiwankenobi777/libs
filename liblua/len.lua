8305b976ab3a6c3e76abf051f29e178f610142ba
obiwankenobi777 <ordemjedi7@gmail.com>
2025-11-16 21:16:28 -0300

    len.lua

--- liblua/len.lua
+++ liblua/len.lua
@@ -1,18 +1,19 @@
 #!/usr/bin/lua
 
 function main()
-    if #arg < 1 then
+    if #arg == 0 then
         print("Missing Arguments")
         return 1
     end
 
     local chars, words = 0, 0
     for i = 1, #arg do
-        io.write("["..i.."] ".. arg[i].." -> "..#arg[i].."\n")
+        io.write("["..i.."] " .. arg[i].." -> " .. #arg[i] .. "\n")
         chars = chars + #arg[i]
         words = words + 1
     end
-    io.write("\nWords -> "..words)
-    io.write("\nCharcaters -> " .. chars .. "\n")
+    io.write("\nWords -> " .. words)
+    io.write("\nCharacters -> " .. chars .. "\n")
 end
+
 main()
