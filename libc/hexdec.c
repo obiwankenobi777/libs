1247061d57d26f208f44bfb0be1ce921ac451829
obiwankenobi777 <ordemjedi7@gmail.com>
2026-02-14 11:17:26 -0300

    new features

--- libc/hexdec.c
+++ libc/hexdec.c
@@ -3,10 +3,16 @@
 int main(int argc, char *argv[]) {
     for (int i = 1; argv[i]; ++i) {
         printf("%s em hex: ", argv[i]);
+
         for (int j = 0; argv[i][j] != '\0'; ++j) {
             printf("%02x ", (unsigned char)argv[i][j]);
         }
-        putchar('\n');
+
+        printf("\n%s em dec: ", argv[i]);
+        for (int j = 0; argv[i][j] != '\0'; ++j) {
+            printf("%d ", (unsigned char)argv[i][j]);
+        }
+        printf("\n\n");
     }
     return 0;
 }
